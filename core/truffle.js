c5b1b1d6ab1df4e622f6c6a426b998e5411a6c87
Hadrien Croubois <hc@iex.ec>
2020-01-12 23:32:13 +0100

    core cleanup

--- core/truffle.js
+++ core/truffle.js
@@ -1,5 +1,3 @@
-var HDWalletProvider = require("@truffle/hdwallet-provider");
-
 module.exports =
 {
 	plugins: [ "truffle-security" ],
@@ -19,12 +17,6 @@ module.exports =
 			network_id: "*",
 			gas:        0xFFFFFFFFFFF, // <-- Use this high gas value
 			gasPrice:   0x01           // <-- Use this low gas price
-		},
-		goerli:
-		{
-			provider: () => new HDWalletProvider(process.env.MNEMONIC, process.env.GOERLI_NODE),
-			network_id: '5',
-			gasPrice:   22000000000, //22Gwei
 		}
 	},
 	compilers: {
